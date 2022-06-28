dcecdd5c3655f6410f81bda5d0042ae396e52f55
Annie Lin <yl725@duke.edu>
2022-06-28 16:59:43 -0400

    day3

--- adopt-me/src/App.jsx
+++ adopt-me/src/App.jsx
@@ -1,14 +1,15 @@
 import { render } from "react-dom";
-import Pet from "./Pet"
+import SearchParams from "./SearchParams";
+import { StrictMode } from "react";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="dog" breed="Havanese" />
-      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="cat" breed="Mix" />
-    </div>
+    <StrictMode>
+      <div>
+        <h1>Adopt Me!</h1>
+        <SearchParams />
+      </div>
+    </StrictMode>
   );
 };
 
