e73b5293bb9e38e8f5aa759c8154ea7f50fbf9a1
Annie Lin <yl725@duke.edu>
2022-06-29 16:04:16 -0400

    day4

--- adopt-me/src/App.jsx
+++ adopt-me/src/App.jsx
@@ -1,14 +1,21 @@
 import { render } from "react-dom";
 import SearchParams from "./SearchParams";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Details from "./Details";
 import { StrictMode } from "react";
 
 const App = () => {
   return (
     <StrictMode>
-      <div>
-        <h1>Adopt Me!</h1>
-        <SearchParams />
-      </div>
+      <BrowserRouter>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
     </StrictMode>
   );
 };
